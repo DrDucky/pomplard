03e488dd7b513823bacdaf4bf9cbbb478f37ed5f
Mecatti <loic.mecatti@capgemini.com>
2018-04-25 16:30:38 +0200

    Refactoring pomplarg to pomplard app

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# pomplarg
+# pomplard
--- app/src/androidTest/java/com/loic/pomplard/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/loic/pomplard/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.loic.pomplarg", appContext.packageName)
+        assertEquals("com.loic.pomplard", appContext.packageName)
     }
 }
